871d25a647e71eeefd59ac71159631c16f41d78a
vagusx <vagusxl@gmail.com>
2022-04-20 21:00:56 +0800

    refactor: cssinjs for segmented

--- components/_util/theme/interface.ts
+++ components/_util/theme/interface.ts
@@ -6,6 +6,7 @@ import type { ComponentToken as EmptyComponentToken } from '../../empty/style';
 import type { ComponentToken as CascaderComponentToken } from '../../cascader/style';
 import type { ComponentToken as InputNumberComponentToken } from '../../input-number/style';
 import type { ComponentToken as MentionsComponentToken } from '../../mentions/style';
+import type { ComponentToken as SegmentedComponentToken } from '../../segmented/style';
 import type { ComponentToken as SelectComponentToken } from '../../select/style';
 import type { ComponentToken as SliderComponentToken } from '../../slider/style';
 import type { ComponentToken as TypographyComponentToken } from '../../typography/style';
@@ -71,6 +72,7 @@ export interface OverrideToken {
   Popover?: {};
   Rate?: {};
   Result?: {};
+  Segmented?: SegmentedComponentToken;
   Select?: SelectComponentToken;
   Skeleton?: {};
   Slider?: SliderComponentToken;
@@ -313,6 +315,7 @@ export interface AliasToken extends Omit<DerivativeToken, OmitDerivativeKey> {
 
   controlPaddingHorizontal: number;
   controlPaddingHorizontalSM: number;
+
   paddingSM: number;
   paddingXS: number;
   paddingXXS: number;
--- components/segmented/index.tsx
+++ components/segmented/index.tsx
@@ -10,6 +10,8 @@ import type {
 import { ConfigContext } from '../config-provider';
 import SizeContext, { SizeType } from '../config-provider/SizeContext';
 
+import useStyle from './style';
+
 export type { SegmentedValue } from 'rc-segmented';
 
 export interface SegmentedLabeledOption extends RcSegmentedLabeledOption {
@@ -37,6 +39,8 @@ const Segmented = React.forwardRef<HTMLDivElement, SegmentedProps>((props, ref) 
 
   const { getPrefixCls, direction } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('segmented', customizePrefixCls);
+  // Style
+  const [wrapSSR, hashId] = useStyle(prefixCls);
 
   // ===================== Size =====================
   const size = React.useContext(SizeContext);
@@ -63,19 +67,23 @@ const Segmented = React.forwardRef<HTMLDivElement, SegmentedProps>((props, ref) 
     [options, prefixCls],
   );
 
-  return (
+  return wrapSSR(
     <RcSegmented
       {...restProps}
-      className={classNames(className, {
-        [`${prefixCls}-block`]: block,
-        [`${prefixCls}-sm`]: mergedSize === 'small',
-        [`${prefixCls}-lg`]: mergedSize === 'large',
-      })}
+      className={classNames(
+        className,
+        {
+          [`${prefixCls}-block`]: block,
+          [`${prefixCls}-sm`]: mergedSize === 'small',
+          [`${prefixCls}-lg`]: mergedSize === 'large',
+        },
+        hashId,
+      )}
       options={extendedOptions}
       ref={ref}
       prefixCls={prefixCls}
       direction={direction}
-    />
+    />,
   );
 });
 
